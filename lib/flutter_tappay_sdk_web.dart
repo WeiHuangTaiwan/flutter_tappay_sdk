0c0d22fc29c06f5b5a8ec48e367e2f188ffed920
黃偉（WEI HUANG） <131838331+WeiHuangTaiwan@users.noreply.github.com>
2025-05-04 22:28:03 +0800

    Update flutter_tappay_sdk_web.dart
    null fix

--- lib/flutter_tappay_sdk_web.dart
+++ lib/flutter_tappay_sdk_web.dart
@@ -27,8 +27,11 @@ class FlutterTapPaySdkWeb extends MethodChannelFlutterTapPaySdk {
 
   @override
   Future<String> getPrime() async {
-    final prime = await TPDirect.card.getPrime(); // 舉例
-    return prime!.prime; // 用 ! 保證它不為 null（如果你已驗證安全）
+    final prime = await FlutterTappaySdkWebImpl().getPrime();
+    if (prime == null) {
+      throw Exception('getPrime failed: prime is null');
+    }
+    return prime;
   }
 
   @override
